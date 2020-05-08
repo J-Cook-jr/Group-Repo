d7ef2c380e2466417f2006262e7b2eaf12a1fd94
Zahria Shannon <zahria.shannon08@gmail.com>
2020-05-08 13:41:30 -0400

    changes

--- script/index.js
+++ script/index.js
@@ -61,6 +61,7 @@ search.addEventListener('submit', function(e) {
         strainList.innerHTML = strainHtmlArray.join('')
         strainArray = strainData;
         console.log(strainArray);
+        
         // console.log(movieData);
     })
 
